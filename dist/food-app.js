53ce46f26b4522bf479b8746a0e8f928cd4757d9
daideguchi <dd.1107.11107@gmail.com>
2022-11-03 12:16:09 +0900

    221103

--- dist/food-app.js
+++ dist/food-app.js
@@ -1,5 +1,19 @@
 "use strict";
 class Score {
+    constructor() { }
+    get totalScore() {
+        const foods = Foods.getInstance();
+        return foods.activeElementsScore.reduce((total, score) => total + score, 0);
+    }
+    render() {
+        document.querySelector(".score__number").textContent = String(this.totalScore);
+    }
+    static getInstance() {
+        if (!Score.instance) {
+            Score.instance = new Score();
+        }
+        return Score.instance;
+    }
 }
 class Food {
     constructor(element) {
@@ -8,20 +22,22 @@ class Food {
     }
     clickEventHandler() {
         this.element.classList.toggle("food--active");
+        const score = Score.getInstance();
+        score.render();
     }
 }
 class Foods {
     constructor() {
-        this.elements = document.querySelectorAll('.food');
+        this.elements = document.querySelectorAll(".food");
         this._activeElements = [];
         this._activeElementsScore = [];
-        this.elements.forEach(element => {
+        this.elements.forEach((element) => {
             new Food(element);
         });
     }
     get activeElements() {
         this._activeElements = [];
-        this.elements.forEach(element => {
+        this.elements.forEach((element) => {
             if (element.classList.contains("food--active")) {
                 this._activeElements.push(element);
             }
@@ -30,7 +46,7 @@ class Foods {
     }
     get activeElementsScore() {
         this._activeElementsScore = [];
-        this.activeElements.forEach(element => {
+        this.activeElements.forEach((element) => {
             const foodScore = element.querySelector(".food__score");
             if (foodScore) {
                 this._activeElementsScore.push(Number(foodScore.textContent));
@@ -38,5 +54,11 @@ class Foods {
         });
         return this._activeElementsScore;
     }
+    static getInstance() {
+        if (!Foods.instance) {
+            Foods.instance = new Foods();
+        }
+        return Foods.instance;
+    }
 }
-const foods = new Foods();
+const foods = Foods.getInstance();
